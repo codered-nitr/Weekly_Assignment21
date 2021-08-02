cf06bfa4083e90b5e94e7b9280f96fa53dbc58b8
BGayatri1103 <88179111+BGayatri1103@users.noreply.github.com>
2021-08-02 23:25:11 +0530

    Create 120CE0047_Q2.cpp

---
+++ submissions/120CE0047/120CE0047_Q2.cpp
@@ -0,0 +1,14 @@
+int doUnion(int a[], int n, int b[], int m)  {
+        set<int>s;
+        for(int i=0;i<n;i++)
+        {
+            s.insert(a[i]);
+            
+        }
+         for(int i=0;i<m;i++)
+        {
+            s.insert(b[i]);
+            
+        }
+        return s.size();
+    }
