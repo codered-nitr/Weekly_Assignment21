9292e6a7710e20dede1f66688b17ab474f561b6a
Gourav Mohanty <gouravmohanty2001@gmail.com>
2021-08-10 21:36:59 -0700

    Create 120CR0385_Q4.java

---
+++ submissions/120CR0385/120CR0385_Q4.java
@@ -0,0 +1,11 @@
+   //Used Recursion
+    static int fibonacci(int n){ 
+        if(n<=1) 
+           return n; 
+        return fibonnaci(n-1) + fibonacci(n-2); 
+    } 
+       
+    public static void main (String args[]){ 
+        int m = 12; 
+        System.out.println(fibonacci(n)); 
+    }
