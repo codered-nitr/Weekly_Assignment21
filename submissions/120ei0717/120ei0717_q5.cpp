015e67fffb9c7a98aed2e1e9fdfaf67d93a67fa3
Asish-AP <88313279+Asish-AP@users.noreply.github.com>
2021-08-12 16:59:28 +0530

    Create 120ei0717q1.cpp (#117)
    * Create 120ei0717q1.cpp
    
    * Create 120ei0717_q2.cpp
    
    * Create 120ei0717_q3.cpp
    
    * Create 120ei0717_q4.cpp
    
    * Create 120ei0717_q5.cpp
    
    * Create 120ei0717_q6.cpp
    
    * Create 120ei0717_q7.cpp
    
    * Create 120ei0717_q9.cpp
    
    * Create 120ei0717_q10.cpp

---
+++ submissions/120ei0717/120ei0717_q5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+         int n = nums.size();
+        vector<int> ans(n);
+        vector<int> left(n);
+        vector<int> right(n);
+        
+        left[0] = 1;
+        for(int i = 1; i < nums.size(); i++){
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        
+        right[n - 1] = 1;
+        for (int j = n - 2; j >= 0; j--) {
+            right[j] = right[j + 1] * nums[j + 1];
+        }
+        
+        for (int k = 0; k < n; k++) {
+            ans[k]= left[k] * right[k];
+        }
+        
+        return ans;
+    }
+};
