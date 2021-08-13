6c1f16ce18a01c9c53351700fbcbcb2d5b7cec3c
Ancoco <88350684+Ancoco@users.noreply.github.com>
2021-08-13 16:06:00 +0530

    Create 120CH0379.cpp

---
+++ submissions/120CH0379/120CH0379.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	int j=0;
+	for(int i=0;i<n;i++)
+	{
+	    if(a[i]<0 )
+	    {
+	       int t=a[j];
+	       a[j]=a[i];
+	       a[i]=t;
+	       j++;
+	    }
+	}
+	for(int i=0;i<n;i++)
+	cout<<a[i]<<" ";
+	
+	return 0;
+}
