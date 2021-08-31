a26b90b499b32539ac3884ffa357b2b6d57a24ee
Asish-AP <88313279+Asish-AP@users.noreply.github.com>
2021-08-31 17:18:49 +0530

    Create 120eio717q2.cpp

---
+++ submissions/120ei0717/120eio717q2.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+          vector<long long>ans (n);
+        stack<long long> s;
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!s.empty() && s.top()<=arr[i])
+            {
+                s.pop();
+            }
+            ans[i]=s.empty()?-1:s.top();
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
