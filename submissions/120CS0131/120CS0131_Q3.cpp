0a0b3cfebf8b35887b4b5115c3ed019ea17ffb6c
Pulakesh5 <68358402+Pulakesh5@users.noreply.github.com>
2021-08-31 12:21:15 +0530

    Create 120CS0131_Q3.cpp (#215)

---
+++ submissions/120CS0131/120CS0131_Q3.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        unordered_set<int> st;
+        bool flag=false;
+        int sum=0;
+       for(int i=0;i<n;i++)
+       {
+           sum+=arr[i];
+           if(st.find(sum)!=st.end() || sum==0)
+                {
+                    flag=true;
+                    break;
+                }
+            else
+                st.insert(sum);
+       }
+       return flag;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
