7b037c5bf191e73ab17c2f1cce2e9291cb47843d
pratyush507 <83583931+pratyush507@users.noreply.github.com>
2021-09-15 15:36:10 +0530

    Create 720CH1004_Q3.cpp (#231)
    * Create 720CH1004_Q3.cpp
    
    * Create 720CH1004_Q5.cpp

---
+++ submissions/720CH1004/720CH1004_Q3.cpp
@@ -0,0 +1,40 @@
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        unordered_set<int> sumSet;
+    int sum = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        sum += arr[i];
+        if (sum == 0 || sumSet.find(sum) != sumSet.end())
+            return true;
+            sumSet.insert(sum);
+    }
+    return false;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
