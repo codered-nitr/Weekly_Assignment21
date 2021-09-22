f27a2350d7eef912452bc7a5fcb930fd4950ee6a
Priyanshu1609 <79529908+Priyanshu1609@users.noreply.github.com>
2021-09-22 10:27:49 +0530

    Create q6.cpp

---
+++ submissions/q6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int square(int n)
+{
+    if (n < 0)
+        n = -n;
+ 
+    int res = n;
+ 
+    for (int i = 1; i < n; i++)
+        res += n;
+ 
+    return res;
+}
+int main()
+{
+    for (int n = 1; n <= 5; n++)
+        cout << "n = " << n << ", n^2 = " << square(n)
+             << endl;
+    return 0;
+}
