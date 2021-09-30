9da5953171d6fff146c37be8dd82e4cffddd4a08
Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>
2021-09-30 11:23:28 +0530

    feat: Submission for week1 - Q1 (#238)

---
+++ submissions/119CR0087/119CR0087_Q1.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        long long cs = arr[0];
+        long long largest = arr[0];
+        for(int i = 1; i< n; i++){
+            cs = max((long long)arr[i], cs+(long long)arr[i]);
+            largest = max(largest, cs);
+        }
+        return largest;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
